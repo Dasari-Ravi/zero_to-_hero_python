e47352c27ad25fcc7660e6ad7f5b04d641f75f17
Dasari-Ravi <89849794+Dasari-Ravi@users.noreply.github.com>
2021-09-14 20:43:26 +0530

    else statement

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
-print('Hello RAVI');
-a=508
-b=508
-c=a+b
-print(c)
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> t= 12345,56789,3456
+>>> t[0]
